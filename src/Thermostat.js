3d423e4215b49bb9bcbdc1fd0972f907f89d0295
Marco Bagnasco <admin@hecticmojo.com>
2016-04-29 22:10:13 +0100

    Refactoring Thermostat.js

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,25 +2,21 @@
 	
 
 	var Thermostat = function() {
-		var _ = PrivateParts.createKey();
-		_(this).temperature = 20;
-		_(this).MIN_TEMPERATURE = 10;
-		_(this).powerSaving = true;
+		  this.temperature = 20;
+		  this.MIN_TEMPERATURE = 10;
+		  this.powerSaving = true;
+  };
 	
 		Thermostat.prototype.resetTemperature = function() {
-			return _(this).temperature = 20;
+			return this.temperature = 20;
 		}
 	
 		Thermostat.prototype.switchPowerSaving = function() {
-			if (_(this).powerSaving) {
-			   return _(this).powerSaving = false ;
-			} else {
-				return _(this).powerSaving = true
-			};
+			(this.powerSaving == true) ? (this.powerSaving = false) : (this.powerSaving = true)
 		}
 	
 		Thermostat.prototype.isPowerSavingOn = function() {
-			if (_(this).powerSaving) {
+			if (this.powerSaving) {
 				return true ;
 			} else {
 				return false ;
@@ -28,19 +24,19 @@
 	   }
 	
 	   Thermostat.prototype._isMinimumTemp = function() {
-	   	if (_(this).temperature === _(this).MIN_TEMPERATURE) return true ;
+	   	if (this.temperature === this.MIN_TEMPERATURE) return true ;
 	   }
 	
 		Thermostat.prototype.getTemperature = function() {
-			return _(this).temperature;
+			return this.temperature;
 		}
 	
 		Thermostat.prototype.isMaxTempWithPSMon = function() {
-			if (this.isPowerSavingOn() && _(this).temperature === 25) {return true};
+			if (this.isPowerSavingOn() && this.temperature === 25) {return true};
 		}
 	
 		Thermostat.prototype.isMaxTempWithPSMoff = function() {
-			if (!this.isPowerSavingOn() && _(this).temperature === 32) {return true};
+			if (!this.isPowerSavingOn() && this.temperature === 32) {return true};
 		}
 	
 		Thermostat.prototype.raiseTemperature = function() {
@@ -50,7 +46,7 @@
 			   (this.isMaxTempWithPSMoff()) { 
 				throw("Maximum Possible Temperature Reached")	
 			} else {
-				return _(this).temperature += 1;
+				return this.temperature += 1;
 			};	
 		}
 	
@@ -58,24 +54,24 @@
 			if (this._isMinimumTemp()) {	
 				throw("Reached Temperature Limit") 
 			} else {
-			  return _(this).temperature -= 1 
+			  return this.temperature -= 1 
 			};
 	   }
 	
 		Thermostat.prototype.readMinTemperature = function() {
-			return _(this).MIN_TEMPERATURE;
+			return this.MIN_TEMPERATURE;
 		}
 	
 		Thermostat.prototype.display = function() {
-			if (_(this).temperature < 18) {
+			if (this.temperature < 18) {
 				return "low-usage"
-			} else if (_(this).temperature < 25) {
+			} else if (this.temperature < 25) {
 				return "medium-usage"
 			} else {
 				return "High-Usage"
 			};
 		}
 	
-	};
+
 
 
